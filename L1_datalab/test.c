509c6ff7c01a6c94da6ef831e17b204f145d0c6c
Yichuan <wsxdwang@gmail.com>
2018-09-11 03:48:47 -0400

    L1 33/41

--- L1_datalab/test.c
+++ L1_datalab/test.c
@@ -6,15 +6,21 @@ int fitsBits(int x, int n) {
     return result;
 }
 int main () {
-    
-    /* local variable definition */
-    int x =0x80;
-    int b = 0;
-    
-    int ret = fitsBits(0x80000000,32);
-    
-    printf( "Max value is : %d\t %x\n", ret,ret );
-    
+	  int result,x;
+	  x=1;
+  result = 0;
+printf("%d\n",result);    
+  result = (!!(x>>16))*16;
+printf("%d\n",result);    
+  result = result + (!!(x>>(8+result)))*8;
+printf("%d\n",result);    
+  result = result +(!!(x>>(4+result)))*4;
+printf("%d\n",result);    
+  result = result +(!!(x>>(2+result)))*2;
+printf("%d\n",result);    
+  result = result +(!!(x>>(1+result)));
+printf("%d\n",result);    
+
     return 0;
 }
 
